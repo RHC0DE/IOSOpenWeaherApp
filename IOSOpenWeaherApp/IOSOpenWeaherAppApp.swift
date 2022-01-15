78b9216d7fc203913bbf3976c0487aecfd0e0393
Ryno-Haakmat <58630159+Ryno-Haakmat@users.noreply.github.com>
2022-01-15 21:05:42 +0100

    Initial Commit

---
+++ IOSOpenWeaherApp/IOSOpenWeaherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOSOpenWeaherAppApp.swift
+//  IOSOpenWeaherApp
+//
+//  Created by Guest User on 15/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct IOSOpenWeaherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
